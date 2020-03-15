f5251206ce5b111e700a6cd15b4f89b8a63ba720
Manila Bui <manila.bui@gmail.com>
2020-03-15 18:16:53 -0500

    updated skillager avatar to blank=true

--- skillchill/skillchillapi/models/skillager.py
+++ skillchill/skillchillapi/models/skillager.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Skillager(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.CharField(max_length=128, default='http://placekitten.com/200/200')
+    avatar = models.CharField(max_length=128, blank=True)
 
     class Meta:
         verbose_name = ("skillager")
