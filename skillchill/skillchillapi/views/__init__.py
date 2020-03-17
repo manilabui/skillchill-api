71b41b606951d5e9bc2f228b0e98274bd88aed29
Manila Bui <manila.bui@gmail.com>
2020-03-17 13:16:20 -0500

    post page url + view + model update

--- skillchill/skillchillapi/views/__init__.py
+++ skillchill/skillchillapi/views/__init__.py
@@ -2,3 +2,4 @@ from .register import login_user, register_user
 from .skill import Skills
 from .skillager import Skillagers
 from .post import Posts
+from .post_page import PostPages
--- skillchill/skillchillapi/views/post_page.py
+++ skillchill/skillchillapi/views/post_page.py
@@ -2,58 +2,44 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from skillchillapi.models import Post
-from .skillager import SkillagersSerializer
-from .skill import SkillsSerializer
+from skillchillapi.models import PostPage
 
 
-# TODO: need to show the moderator + all members
-class PostsSerializer(serializers.HyperlinkedModelSerializer):
+class PostPagesSerializer(serializers.HyperlinkedModelSerializer):
     """JSON serializer for posts
 
     Arguments:
         serializers
     """
-    skillager = SkillagersSerializer()
-    skill = SkillsSerializer()
-    post_type = serializers.CharField(source='get_post_type_display')
-
     class Meta:
-        model = Post
+        model = PostPage
         url = serializers.HyperlinkedIdentityField(
-            view_name='post',
+            view_name='postpage',
             lookup_field='id'
         )
-
-        fields = ('id', 'skillager', 'skill', 'post_type',
-                  'is_public', 'created_at', 'modified_at')
+        fields = ('id', 'post', 'content', 'caption',
+                  'page_num', 'created_at', 'modified_at')
         depth = 2
 
-    def get_post_type(self, obj):
-        return obj.get_post_type_display()
 
-
-class Posts(ViewSet):
+class PostPages(ViewSet):
     """Skills in Skillchill"""
-
     def create(self, request):
         """Handle POST operations
 
         Returns:
             Response -- JSON serialized postinstance
         """
-        new_post = Post()
-        new_post.skillager_id = request.auth.user.skillager.id
-        new_post.skill_id = request.data['skill_id']
-        new_post.post_type = request.data['post_type']
-        new_post.is_public = request.data['is_public']
-        new_post.save()
-
-        serializer = PostsSerializer(
-            new_post,
+        new_post_page = PostPage()
+        new_post_page.post_id = request.data['post_id']
+        new_post_page.content = request.data['content']
+        new_post_page.caption = request.data['caption']
+        new_post_page.page_num = request.data['page_num']
+        new_post_page.save()
+        serializer = PostPagesSerializer(
+            new_post_page,
             context={'request': request}
         )
-
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
@@ -63,8 +49,8 @@ class Posts(ViewSet):
             Response -- JSON serialized post instance
         """
         try:
-            post = Post.objects.get(pk=pk)
-            serializer = PostsSerializer(post, context={'request': request})
+            post_page = PostPage.objects.get(pk=pk)
+            serializer = PostPagesSerializer(post_page, context={'request': request})
             return Response(serializer.data)
         except Exception as ex:
             return HttpResponseServerError(ex)
@@ -75,9 +61,10 @@ class Posts(ViewSet):
         Returns:
             Response -- JSON serialized list of posts
         """
-        posts = Post.objects.all()
-        serializer = PostsSerializer(
-            posts,
+        post_id = self.request.query_params.get('post', None)
+        post_pages = PostPage.objects.filter(post__id=post_id)
+        serializer = PostPagesSerializer(
+            post_pages,
             many=True,
             context={'request': request}
         )
@@ -89,11 +76,10 @@ class Posts(ViewSet):
         Returns:
             Response -- Empty body with 204 status code
         """
-        post = Post.objects.get(pk=pk)
-        post.is_public = request.data["is_public"]
-        post.modified_at = request.data["modified_at"]
-        post.save()
-
+        post_page = PostPage.objects.get(pk=pk)
+        post_page.caption = request.data["caption"]
+        post_page.modified_at = request.data["modified_at"]
+        post_page.save()
         return Response({}, status=status.HTTP_204_NO_CONTENT)
 
     def destroy(self, request, pk=None):
@@ -103,15 +89,12 @@ class Posts(ViewSet):
             Response -- 204, 404, or 500 status code
         """
         try:
-            post = Post.objects.get(pk=pk)
-            post.delete()
-
+            post_page = PostPage.objects.get(pk=pk)
+            post_page.delete()
             return Response({}, status=status.HTTP_204_NO_CONTENT)
-
-        except Post.DoesNotExist as ex:
+        except PostPage.DoesNotExist as ex:
             return Response({'message': ex.args[0]},
                             status=status.HTTP_404_NOT_FOUND)
-
         except Exception as ex:
             return Response({'message': ex.arg[0]},
                             status=status.HTTP_500_INTERNAL_SERVER_ERROR)
