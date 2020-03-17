277209091473b4f24c00753a0dbaa92e94f91c0f
Manila Bui <manila.bui@gmail.com>
2020-03-17 14:16:44 -0500

    added userviewset

--- skillchill/skillchill/urls.py
+++ skillchill/skillchill/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.urls import include, path
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
-from skillchillapi.views import register_user, login_user, Posts, PostPages, Skillagers, Skills
+from skillchillapi.views import register_user, login_user, Posts, PostPages, Skillagers, Skills, Users
 
 router = routers.DefaultRouter(trailing_slash=False)
 
@@ -24,6 +24,7 @@ router.register(r'posts', Posts, 'post')
 router.register(r'postpages', PostPages, 'postpage')
 router.register(r'skillagers', Skillagers, 'skillager')
 router.register(r'skills', Skills, 'skill')
+router.register(r'users', Users, 'user')
 
 urlpatterns = [
     path('', include(router.urls)),
